de7c831bd2b64f9d3e1809d69890bc40361ae30b
ShrutiKakade28 <shrutivkakade@gmail.com>
2024-07-06 01:52:20 +0530

    Create square of numbers.cpp
    This is C++ program for square of numbers.

---
+++ square of numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter a  number: ";
+    cin>>a;
+    cout<<"Suare of given two number is: "<<a*a;
+    cin>>a;
+    return 0;
+}

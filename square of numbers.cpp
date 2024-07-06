edaeeb8a8679b3bfafe03aea86f824fa92177e23
ShrutiKakade28 <shrutivkakade@gmail.com>
2024-07-06 17:50:20 +0530

    square of numbers.cpp

--- square of numbers.cpp
+++ square of numbers.cpp
@@ -1,11 +1,1 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    int a;
-    cout<<"Enter a  number: ";
-    cin>>a;
-    cout<<"Square of given two number is: "<<a*a;
-    cin>>a;
-    return 0;
-}
+

b1851209adde405eda77d0498d1ccd8c73142905
ShrutiKakade28 <shrutivkakade@gmail.com>
2024-07-12 18:40:29 +0530

    Create Firstname and lastname in reverse order.cpp

---
+++ Firstname and lastname in reverse order.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string firstname,lastname;
+
+    cout<<"Enter your first name: ";
+    cin>>firstname;
+    cout<<"Enter your last name: ";
+    cin>>lastname;
+
+    cout<<"Your name in reverse order is: "<<lastname<<" "<<firstname<<"\n";
+    return 0;
+}
